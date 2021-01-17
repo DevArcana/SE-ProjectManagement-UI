694d7f25cf52f21cf0fc43ea43b820ca683f5494
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-01-17 16:41:10 +0100

    remove editing issues

--- src/issues/components/IssueInfo.tsx
+++ src/issues/components/IssueInfo.tsx
@@ -7,12 +7,6 @@ import {
   Td,
   useDisclosure,
   Button,
-  Drawer,
-  DrawerOverlay,
-  DrawerBody,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
   Text,
   Select
 } from "@chakra-ui/react";
@@ -23,7 +17,6 @@ interface Props {
 }
 
 export const IssueInfo: React.FC<Props> = props => {
-  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: false });
   const { projectId } = useParams();
   const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
@@ -45,44 +38,10 @@ export const IssueInfo: React.FC<Props> = props => {
     <Tr>
       <Td>
         <Text
-          as={Button}
-          onClick={onOpen}
-          bg="transparent"
-          _hover={{ bg: "secondary.100", fontSize: "lg" }}
         >
           {issue.id}
         </Text>
       </Td>
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        trapFocus={false}
-        useInert={false}
-      >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerHeader color="#fff">Update: {issue.name}</DrawerHeader>
-            <DrawerBody color="#fff">
-              change name, desc, assignee,status - insert IssueUpdate component
-            </DrawerBody>
-            <DrawerFooter>
-              <Button
-                colorScheme="primary"
-                mr={3}
-                color="#fff"
-                type={"submit"}
-                onClick={onClose}
-              >
-                Save
-              </Button>
-              <Button colorScheme="secondary" onClick={onClose} color="#fff">
-                Cancel
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
       <Td>{issue.name}</Td>
       <Td>{issue.description}</Td>
       <Td>
